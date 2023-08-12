4de5ce66607a958407b488cd2c6db96f2af98ac8
Aleh Ladyka <Aleh_Ladyka@epam.com>
2023-08-12 23:57:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+Console.WriteLine(Solution_2023_08_12.LengthOfLastWord("luffy is still joyboy"));
+Console.ReadKey();
---
+++ Solution_2023_08_12.cs
@@ -0,0 +1,30 @@
+﻿public class Solution_2023_08_12
+{
+    public static int LengthOfLastWord(string s)
+    {
+        int result = 0;
+        int currentResult = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] != ' ')
+            {
+                currentResult++;
+            }
+            else
+            {
+                if (currentResult != 0)
+                {
+                    result = currentResult;
+                }
+                currentResult = 0;
+            }
+        }
+
+        if (currentResult != 0)
+        {
+            result = currentResult;
+        }
+
+        return result;
+    }
+}
