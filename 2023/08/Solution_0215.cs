02d6bd186020665f06f08ca02dfd6b4eec9b4303
Aleh Ladyka <Aleh_Ladyka@epam.com>
2023-08-18 20:19:35 +0300

    215. Kth Largest Element in an Array

---
+++ 2023/08/Solution_0215.cs
@@ -0,0 +1,49 @@
+﻿/// <summary>
+/// 215. Kth Largest Element in an Array
+/// Given an integer array nums and an integer k, return the kth largest element in the array.
+/// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+/// Can you solve it without sorting?
+/// </summary>
+internal class Solution_0215
+{
+    public static int FindKthLargest(int[] nums, int k)
+    {
+        int[] sorted = new int[k];
+        int minInSorted = int.MinValue;
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > minInSorted)
+            {
+                minInSorted = PutInSorted(sorted, nums[i], k);
+            }
+        }
+
+        return minInSorted;
+    }
+
+    private static int PutInSorted(int[] sorted, int num, int count)
+    {
+        bool isAdded = false;
+        for (int i = 0; i < count - 1; i++)
+        {
+            if (sorted[i] <= num && sorted[i + 1] > num)
+            {
+                sorted[i] = num;
+                isAdded = true;
+                break;
+            }
+            else
+            {
+                sorted[i] = sorted[i + 1];
+            }
+        }
+
+        if (!isAdded)
+        {
+            sorted[count - 1] = num;
+        }
+
+        return sorted[0];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,4 +3,16 @@
 //Console.WriteLine(!Solution_0009.IsPalindrome(-121));
 //Console.WriteLine(Solution_0009.IsPalindrome(1221));
 //Console.WriteLine(Solution_58.LengthOfLastWord("luffy is still joyboy") == 6);
+
+
+//Console.WriteLine(Solution_0215.FindKthLargest(new []{ 3, 2, 1, 5, 6, 4 }, 2) == 5);
+//Console.WriteLine(Solution_0215.FindKthLargest(new []{ 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4) == 4);
+
+using System.Diagnostics;
+
+Stopwatch watch = Stopwatch.StartNew();
+Console.WriteLine(Solution_0215.FindKthLargest(new []{ 3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6 }, 20) == 2);
+watch.Stop();
+Console.WriteLine(watch.ElapsedMilliseconds);
+
 Console.ReadKey();
